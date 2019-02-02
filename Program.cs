96208788a623f94eb77e25e6254270a77f5caeb7
GolemProgramer <47268372+GolemProgramer@users.noreply.github.com>
2019-02-02 10:34:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoSkiing.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumSki = int.Parse(Console.ReadLine());
+            int NumjAKET = int.Parse(Console.ReadLine());
+            int NumHELMET = int.Parse(Console.ReadLine());
+            int NumShoe = int.Parse(Console.ReadLine());
+
+            int Jaketprice = NumjAKET * 120;
+            int Helmetprice = NumHELMET * 75;
+            double Shoeprice = NumShoe *  299.9;
+
+            double ONEskip = Jaketprice + Helmetprice + Shoeprice;
+            double AllSki = ONEskip * NumSki;
+
+            double SumDDS = AllSki * 1.2;
+
+            Console.WriteLine("{0:F2}", SumDDS);
+
+        }
+    }
+}
